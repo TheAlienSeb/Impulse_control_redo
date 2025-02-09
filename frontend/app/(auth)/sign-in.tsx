ba6e055113a7255366491e58ecc534fdb453dd92
Haasil Pujara <haasil@MacBook-Pro.local>
2025-02-08 22:53:25 -0500

    added small details to sign-up page, created sign-in page

--- frontend/app/(auth)/sign-in.tsx
+++ frontend/app/(auth)/sign-in.tsx
@@ -1,12 +1,119 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native';
+import { Text, ScrollView, View, ImageBackground, StyleSheet, TextInput, Button } from 'react-native';
+import { addWhitelistedNativeProps } from 'react-native-reanimated/lib/typescript/ConfigHelper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Link, Redirect } from "expo-router";
+import {router} from "expo-router"
 
 const SignIn = () => {
     return (
-        <SafeAreaView>
-            <Text>Sign in</Text>
-        </SafeAreaView>
+        <ImageBackground 
+            source={require('../../assets/images/background.png')} 
+            style={styles.background}
+        >
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                <View style={styles.logoContainer}>
+                    <ImageBackground 
+                        source={require('../../assets/images/logo.png')} 
+                        style={styles.logo}
+                    />
+                    <Text style={styles.text}>SmartSave</Text>
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.smallText}>Username</Text>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Enter your email address..." 
+                        placeholderTextColor="white"
+                    />
+                    <Text style={styles.smallText}>Password</Text>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Enter your password..." 
+                        placeholderTextColor="white"
+                        
+                    />
+                    <TouchableOpacity style={styles.buttonStyle} onPress={() => { /* Handle sign up */ }}>
+                        <Text style={styles.buttonText}>Sign In</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => {
+                            router.replace('/(auth)/sign-up')
+                        }}
+
+                        style={styles.buttonStyle}
+                    >
+                        <Text style={styles.buttonText}>Create New Account</Text> 
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </ImageBackground>
     );
 };
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    scrollViewContent: {
+        marginTop: 100,
+        flexGrow: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    logoContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    logo: {
+        width: 100, 
+        height: 100,
+        marginBottom: 20, 
+    },
+    text: {
+        color: 'white',
+        fontSize: 50,
+        fontWeight: '600',
+    },
+    smallText: {
+        color: 'white',
+        fontSize: 17,
+        fontWeight: '600',
+        alignSelf: 'flex-start',
+        marginLeft: '1%',
+    },
+    inputContainer: {
+        width: '80%',
+        alignItems: 'center',
+        flex: 1,
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#1F2937',
+        borderRadius: 9999,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        color: 'white',
+    },
+    buttonStyle: {
+        width: '80%',
+        alignItems: 'center',
+        marginVertical: 10,
+        backgroundColor: '#0369A1',
+        height: 55,
+        borderRadius: 9999,
+        justifyContent: 'center',
+        marginTop: 15,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 17,
+        fontWeight: '600',
+    },
+});
 
 export default SignIn;
