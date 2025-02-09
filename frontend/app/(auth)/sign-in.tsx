4d265e19f8c2d03bedbeb5c1794e35c5ad542afc
SelinaZheng <selinazhenglx@gmail.com>
2025-02-09 05:23:08 -0500

    changed css in sign in and sign ups

--- frontend/app/(auth)/sign-in.tsx
+++ frontend/app/(auth)/sign-in.tsx
@@ -1,28 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, Text, ScrollView, View, ImageBackground, StyleSheet, TextInput, Alert } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+    TouchableOpacity,
+    Text,
+    ScrollView,
+    View,
+    ImageBackground,
+    StyleSheet,
+    TextInput,
+    Alert,
+} from "react-native";
 import { router } from "expo-router";
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Import images
 import backgroundImage from "../../assets/images/background.png";
-import logoImage from '../../assets/images/logo.png';
+import logoImage from "../../assets/images/logo.png";
+import colors from "../styles/globalVar";
 
 const SignIn = () => {
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
     const [loading, setLoading] = useState(true); // Tracks if session is being checked
 
     useEffect(() => {
         const checkUserSession = async () => {
             try {
-                const storedUser = await AsyncStorage.getItem('user');
+                const storedUser = await AsyncStorage.getItem("user");
                 if (storedUser) {
-                    router.replace('/home'); // Redirect if user is already signed in
+                    router.replace("/home"); // Redirect if user is already signed in
                     return;
                 }
             } catch (error) {
-                console.error('Error checking user session:', error);
+                console.error("Error checking user session:", error);
             }
             setLoading(false); // Stop loading once check is complete
         };
@@ -32,61 +42,65 @@ const SignIn = () => {
 
     const handleSignIn = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/api/signin', {
-                email: userEmail,
-                password: userPassword
-            });
+            const response = await axios.post(
+                "http://localhost:5000/api/signin",
+                {
+                    email: userEmail,
+                    password: userPassword,
+                }
+            );
 
             const userData = response.data.user;
-            await AsyncStorage.setItem('user', JSON.stringify(userData)); // Store session
-            Alert.alert('Success', 'Signed in successfully!');
-            router.replace('../(root)/(tabs)/home'); // Redirect to home
-            
+            await AsyncStorage.setItem("user", JSON.stringify(userData)); // Store session
+            Alert.alert("Success", "Signed in successfully!");
+            router.replace("../(root)/(tabs)/home"); // Redirect to home
         } catch (error) {
-            console.error('Signin error:', error.response?.data || error.message);
-            Alert.alert('Error', 'Invalid credentials');
+            console.error(
+                "Signin error:",
+                error.response?.data || error.message
+            );
+            Alert.alert("Error", "Invalid credentials");
         }
     };
 
     return (
-        <ImageBackground 
-            source={backgroundImage} 
-            style={styles.background}
-        >
+        <ImageBackground source={backgroundImage} style={styles.background}>
             <ScrollView contentContainerStyle={styles.scrollViewContent}>
                 <View style={styles.logoContainer}>
-                    <ImageBackground 
-                        source={logoImage} 
-                        style={styles.logo}
-                    />
+                    <ImageBackground source={logoImage} style={styles.logo} />
                     <Text style={styles.text}>SmartSave</Text>
                 </View>
                 <View style={styles.inputContainer}>
                     <Text style={styles.smallText}>Email</Text>
-                    <TextInput 
-                        style={styles.input} 
-                        placeholder="Enter your email address..." 
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter your email address..."
                         placeholderTextColor="white"
                         value={userEmail}
                         onChangeText={setUserEmail}
                     />
                     <Text style={styles.smallText}>Password</Text>
-                    <TextInput 
-                        style={styles.input} 
-                        placeholder="Enter your password..." 
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter your password..."
                         placeholderTextColor="white"
                         secureTextEntry
                         value={userPassword}
                         onChangeText={setUserPassword}
                     />
-                    <TouchableOpacity style={styles.buttonStyle} onPress={handleSignIn}>
+                    <TouchableOpacity
+                        style={styles.buttonStyle}
+                        onPress={handleSignIn}
+                    >
                         <Text style={styles.buttonText}>Sign In</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity 
-                        onPress={() => router.replace('/(auth)/sign-up')}
+                    <TouchableOpacity
+                        onPress={() => router.replace("/(auth)/sign-up")}
                         style={styles.buttonStyle}
                     >
-                        <Text style={styles.buttonText}>Create New Account</Text> 
+                        <Text style={styles.buttonText}>
+                            Create New Account
+                        </Text>
                     </TouchableOpacity>
                 </View>
             </ScrollView>
@@ -97,69 +111,69 @@ const SignIn = () => {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
     scrollViewContent: {
-        marginTop: 100,
+        marginTop: "15%",
         flexGrow: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        justifyContent: "flex-start",
+        alignItems: "center",
     },
     logoContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 10,
     },
     logo: {
-        width: 100, 
+        width: 100,
         height: 100,
-        marginBottom: 20, 
+        marginBottom: 10,
     },
     text: {
-        color: 'white', 
+        color: "white",
         fontSize: 50,
-        fontWeight: '600',
+        fontWeight: "600",
     },
     smallText: {
-        color: 'white',
-        fontSize: 17,
-        fontWeight: '600',
-        alignSelf: 'flex-start',
-        marginLeft: '1%',
+        color: "white",
+        fontSize: colors.text,
+        fontWeight: "600",
+        alignSelf: "flex-start",
+        marginLeft: "1%",
     },
     inputContainer: {
-        width: '80%',
-        alignItems: 'center',
+        width: "80%",
+        alignItems: "center",
         flex: 1,
+        padding: 10,
     },
     input: {
-        width: '100%',
+        width: "100%",
         height: 50,
-        backgroundColor: '#1F2937',
-        borderRadius: 9999,
+        backgroundColor: colors.secondaryColor,
+        borderRadius: 10,
         paddingHorizontal: 10,
-        marginVertical: 10,
-        color: 'white',
+        marginVertical: 5,
+        color: "white",
     },
     buttonStyle: {
-        width: '80%',
-        alignItems: 'center',
-        marginVertical: 10,
-        backgroundColor: '#0369A1',
-        height: 55,
-        borderRadius: 9999,
-        justifyContent: 'center',
+        width: "80%",
+        alignItems: "center",
+        backgroundColor: colors.primaryColor,
+        borderRadius: 50,
+        justifyContent: "center",
         marginTop: 15,
+        padding: 10,
     },
     buttonText: {
-        color: 'white',
-        fontSize: 17,
-        fontWeight: '600',
+        color: "white",
+        fontSize: colors.text,
+        fontWeight: "600",
     },
     forgotPasswordText: {
-        color: '#0369A1'
-    }
+        color: colors.primaryColor,
+    },
 });
 
 export default SignIn;
