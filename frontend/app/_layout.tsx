d954bbfb67c98b53c3096dd35bca19f81254fd7c
TheAlienSeb <Sebastianramos5387@gmail.com>
2025-02-08 21:58:33 -0500

    fixing stuff maybe

---
+++ frontend/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import {useFonts} from 'expo-font';
+import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+import 'react-native-reanimated';
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout()
+{
+
+return (
+  <Stack>
+    <Stack.Screen name="index" options={ { headerShown: false}}/>
+    <Stack.Screen name="(root)" options={ { headerShown: false}}/>
+    <Stack.Screen name="(auth)" options={ { headerShown: false}}/>
+  </Stack>
+)
+}
